7fd298ae3860dfb55a1bdbad25903f2570a92ebd
llms26 <mxylms1210@163.com>
2024-12-16 23:59:24 +0800

    [update] complete up to ex12

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -3,15 +3,17 @@
 // READ: Trivial type <https://learn.microsoft.com/zh-cn/cpp/cpp/trivial-standard-layout-and-pod-types?view=msvc-170>
 
 struct FibonacciCache {
-    unsigned long long cache[16];
-    int cached;
+    unsigned long long cache[16]{0, 1, 1};
+    int cached = 2;
 };
 
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    int cached = cache.cached;
+    for (;cached <= i; ++cached) {
+        cache.cache[cached] = cache.cache[cached - 1] + cache.cache[cached - 2];
     }
+    cache.cached = cached - 1;
     return cache.cache[i];
 }
 
