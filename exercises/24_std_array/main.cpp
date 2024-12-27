913d61099ee39de4168bf9292fdba51e0ebacff6
llms26 <mxylms1210@163.com>
2024-12-27 15:17:05 +0800

    [update] complete to ex25

--- exercises/24_std_array/main.cpp
+++ exercises/24_std_array/main.cpp
@@ -11,18 +11,18 @@ int main(int argc, char **argv) {
         ASSERT(arr.size() == 5, "Fill in the correct value.");
         ASSERT(sizeof(arr) == 20, "Fill in the correct value.");
         int ans[]{1, 2, 3, 4, 5};
-        ASSERT(std::memcmp(arr.size(), ans, ) == 0, "Fill in the correct values.");
+        ASSERT(std::memcmp(arr.data(), ans, 20) == 0, "Fill in the correct values.");
     }
     {
         std::array<double, 8> arr;
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
+        ASSERT(arr.size() == 8, "Fill in the correct value.");
+        ASSERT(sizeof(arr) == 64, "Fill in the correct value.");
     }
     {
         std::array<char, 21> arr{"Hello, InfiniTensor!"};
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
-        ASSERT(std::strcmp(arr.?, "Hello, InfiniTensor!") == 0, "Fill in the correct value.");
+        ASSERT(arr.size() == 21, "Fill in the correct value.");
+        ASSERT(sizeof(arr) == 21, "Fill in the correct value.");
+        ASSERT(std::strcmp(arr.data(), "Hello, InfiniTensor!") == 0, "Fill in the correct value.");
     }
     return 0;
 }
